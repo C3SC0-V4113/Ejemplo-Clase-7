74dcce0d18a8bfadd20e8915715aab773182dbde
Francisco Valle <vallecesco@gmail.com>
2021-09-26 19:13:50 -0600

    The good News are, Its working

--- navigate/App.js
+++ navigate/App.js
@@ -1,20 +1,40 @@
 import * as React from 'react';
-import { Text, View,Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Text, View, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {Card} from 'react-native-elements';
 
 // npx react-native run-android
 // npx @react-native-community/cli doctor
 //expo start --clear
 //keytool -genkey -v -keystore my-upload-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
 
+const users = [
+  {
+    name: 'brynn',
+    avatar: 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-10-datos-curiosos-sobre-los-gatos.png?itok=88pMyzkl',
+  },
+  {
+    name: 'brynn',
+    avatar: 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-10-datos-curiosos-sobre-los-gatos.png?itok=88pMyzkl',
+  },
+  {
+    name: 'brynn',
+    avatar: 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-10-datos-curiosos-sobre-los-gatos.png?itok=88pMyzkl',
+  },
+  {
+    name: 'brynn',
+    avatar: 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-10-datos-curiosos-sobre-los-gatos.png?itok=88pMyzkl',
+  },
+  // more users here
+];
 
 function Feed() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Image
-        style={{width:600, height: 240, resizeMode: 'stretch'}}
+        style={{width: 600, height: 240, resizeMode: 'stretch'}}
         source={{
           uri: 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-10-datos-curiosos-sobre-los-gatos.png?itok=88pMyzkl',
         }}
@@ -33,17 +53,26 @@ function Feed() {
 
 function Profile() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Profile!</Text>
     </View>
   );
 }
 
 function Notifications() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
-    </View>
+  return(
+  <Card>
+    <Card.Title>CARD WITH DIVIDER</Card.Title>
+    <Card.Divider />
+    {users.map((u, i) => {
+      return (
+        <View key={i}>
+          <Image style={{width: 600, height: 240, resizeMode: 'stretch'}} resizeMode="cover" source={{uri: u.avatar}} />
+          <Text>{u.name}</Text>
+        </View>
+      );
+    })}
+  </Card>
   );
 }
 
@@ -55,14 +84,13 @@ function MyTabs() {
       initialRouteName="Feed"
       screenOptions={{
         tabBarActiveTintColor: '#e91e63',
-      }}
-    >
+      }}>
       <Tab.Screen
         name="GATOS"
         component={Feed}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <Icon name="home" color={color} size={size} />
           ),
         }}
@@ -72,7 +100,7 @@ function MyTabs() {
         component={Notifications}
         options={{
           tabBarLabel: 'Gatos',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <Icon name="paw" color={color} size={size} />
           ),
         }}
@@ -82,7 +110,7 @@ function MyTabs() {
         component={Profile}
         options={{
           tabBarLabel: 'Acerca',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <Icon name="male" color={color} size={size} />
           ),
         }}
